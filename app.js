01866dc2b9684c97431671b94d93ca1185010048
corakots1021 <144332908+corakots1021@users.noreply.github.com>
2023-11-04 20:13:58 +1000

    removing sqs codes

--- app.js
+++ app.js
@@ -19,12 +19,8 @@ const s3 = new AWS.S3({
   region: "ap-southeast-2",
 });
 
-var sqs = new AWS.SQS({apiVersion: '2012-11-05', region: "ap-southeast-2"});
-
 // Define S3 bucket name
 const bucketName = 'cloudproject83';
-
-const queueUrl = 'https://sqs.ap-southeast-2.amazonaws.com/901444280953/cloudproject83';
 
 // Handle file compression and upload
 app.post('/compress', upload.single('file'), (req, res) => {
@@ -33,7 +29,7 @@ app.post('/compress', upload.single('file'), (req, res) => {
   }
 
   const originalFilePath = req.file.path;
-  let compressedFilePath; 
+  let compressedFilePath;
 
   // Use try-catch to handle errors during file compression
   try {
@@ -46,67 +42,36 @@ app.post('/compress', upload.single('file'), (req, res) => {
     // Handle errors during compression
     return res.status(500).send('Error during compression: ' + err.message);
   }
+  // Upload the compressed file to AWS S3
+  const s3Params = {
+    Bucket: bucketName,
+    Key: req.file.originalname + '.bz2', // Use the original filename
+    Body: fs.createReadStream(compressedFilePath), // Use the compressed file
+  };
 
-    // Send a message to the user's SQS queue to handle the upload
-    const message = {
-      originalFileName: req.file.originalname,
-      compressedFilePath: compressedFilePath,
-    };
+  s3.upload(s3Params, (s3Err, s3Data) => {
+    if (s3Err) {
+      console.error('Error uploading file to S3:', s3Err);
+      res.status(500).send('Error uploading file to S3');
+    } else {
+      console.log('File uploaded to S3:', s3Data.Location);
 
-    const params = {
-      MessageBody: JSON.stringify(message),
-      QueueUrl: queueUrl,
-    };
+      // Cleanup: Remove the temporary compressed file and the original file
+      fs.unlinkSync(compressedFilePath);
 
-    sqs.sendMessage(params, (err, data) => {
-      if (err) {
-        console.error('Error sending message to SQS:', err);
-        res.status(500).send('Error sending message to SQS');
-      } else {
-        console.log('Message sent to SQS:', data.MessageId);
+      fs.unlink(originalFilePath, (unlinkErr) => {
+        if (unlinkErr) {
+          console.error('Error deleting the original uploaded file:', unlinkErr);
+        } else {
+          console.log('Original uploaded file deleted.');
+        }
+      });
 
-        // Upload the compressed file to AWS S3
-        const s3Params = {
-          Bucket: bucketName,
-          Key: req.file.originalname + '.bz2', // Use the original filename
-          Body: fs.createReadStream(compressedFilePath), // Use the compressed file
-        };
-
-        s3.upload(s3Params, (s3Err, s3Data) => {
-          if (s3Err) {
-            console.error('Error uploading file to S3:', s3Err);
-            res.status(500).send('Error uploading file to S3');
-          } else {
-            console.log('File uploaded to S3:', s3Data.Location);
-
-            // Cleanup: Remove the temporary compressed file and the original file
-            fs.unlinkSync(compressedFilePath);
-
-            fs.unlink(originalFilePath, (unlinkErr) => {
-              if (unlinkErr) {
-                console.error('Error deleting the original uploaded file:', unlinkErr);
-              } else {
-                console.log('Original uploaded file deleted.');
-              }
-            });
-
-            // Return the S3 URL of the compressed file to the client
-            const s3Url = s3Data.Location;
-            res.status(200).send(s3Url);
-          }
-        });
-      }
-    });
+      // Return the S3 URL of the compressed file to the client
+      const s3Url = s3Data.Location;
+      res.status(200).send(s3Url);
+    }
   });
-
-function generateUserId() {
-  return Math.random().toString(36).substr(2, 9);
-}
-
-// Start listening to messages from the user's SQS queue
-sqs.on('ready', () => {
-  console.log('SQS worker is ready and listening for user-specific messages.');
-  // Replace with logic to process user-specific messages
 });
 
 app.get('/', (req, res) => {
