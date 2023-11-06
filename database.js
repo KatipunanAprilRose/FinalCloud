e7b2d467eb053516458b42a4aad1462e7c5c1add
april rose <aprilrose.katipunan15@gmail.com>
2023-11-06 13:53:49 +1000

    connect db to rds

--- database.js
+++ database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');;
 
 const connection = mysql.createConnection({
-    host:'localhost',
-    database:'test',
-    user: 'root',
-    password:''
+    host     : process.env.RDS_HOSTNAME,
+    user     : process.env.RDS_USERNAME,
+    password : process.env.RDS_PASSWORD,
+    port     : process.env.RDS_PORT,
+    database : process.env.RDS_DATABASE
 })
 
 connection.connect(function(error){
